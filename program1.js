8e450261abd46a6b0dbccf4350ced5a6a74c42b3
Abhijeet Sen <abhijeetsen661@gmail.com>
2024-09-25 12:44:02 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -24,7 +24,7 @@ const getTotalIsles = function (grid) {
   for (let r = 0; r < rows; r++) {
       for (let c = 0; c < cols; c++) {
           if (grid[r][c] === 'L' && !visited[r][c]) {
-              // Found an unvisited land cell, so this is a new island
+              
               islandCount++;
               dfs(r, c); // Perform DFS to mark all connected land
           }
