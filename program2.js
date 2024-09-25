99e2a1011bfc06a976ed12b1e0ddeee6b2ca6fd6
Abhijeet Sen <abhijeetsen661@gmail.com>
2024-09-25 12:41:36 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -11,18 +11,18 @@ const decodeTheRing = function (s, p) {
   
   for (let j = 1; j <= n; j++) {
       if (p[j - 1] === '*') {
-          dp[0][j] = dp[0][j - 1]; // '*' can match an empty message
+          dp[0][j] = dp[0][j - 1]; 
       }
   }
 
-  // Fill the DP table
+  
   for (let i = 1; i <= m; i++) {
       for (let j = 1; j <= n; j++) {
           if (p[j - 1] === '*') {
-              // '*' matches either no characters or one more character in the message
+              
               dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
           } else if (p[j - 1] === '?' || p[j - 1] === s[i - 1]) {
-              // '?' matches any single character or exact character match
+              
               dp[i][j] = dp[i - 1][j - 1];
           }
       }
